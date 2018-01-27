9ac94744a56b8d1cc27ba18cd00ab728909ad94c
osykora <syk.ond@gmail.com>
2018-01-27 23:41:48 +0100

    First C# implementation of Reversed Binary Numbers puzzle

---
+++ C#/ReversedBinaryNumbers/ReversedBinaryNumbers/Reverter.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReversedBinaryNumbers
+{
+    public class Reverter
+    {
+        public int Revert(int value)
+        {
+            return RevertFirstImplementation(value);
+        }
+
+        private int RevertFirstImplementation(int value)
+        {
+            if (value < 1 || value > 1000000000)
+            {
+                throw new ArgumentOutOfRangeException("value");
+            }
+
+            var byteList = new List<int>();
+            var newValue = value;
+
+            while (newValue > 0)
+            {
+                var tempValue = newValue / 2;
+                var remain = newValue % 2;
+                newValue = tempValue;
+
+                byteList.Add(remain);
+            }
+
+            var byteArray = byteList.ToArray();
+
+            var revertedValue = 0;
+            var power = 1;
+            for (int i = byteArray.Length - 1; i >= 0; i--)
+            {
+                revertedValue += byteArray[i] * power;
+                power *= 2;
+            }
+
+            return revertedValue;
+        }
+    }
+}
---
+++ C#/ReversedBinaryNumbers/ReversedBinaryNumbersTest/ReverterTest.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Shouldly;
+using System.Linq;
+using System.Collections.Generic;
+using ReversedBinaryNumbers;
+
+namespace ReversedBinaryNumbersTest
+{
+    [TestClass]
+    public class ReverterTest
+    {
+        [TestMethod]
+        public void GivenZero_WhenRevert_ThenArgumentOutOfRangeExceptionShouldBeThrown()
+        {
+            var reverter = new Reverter();
+
+            Should.Throw<ArgumentOutOfRangeException>(() => reverter.Revert(0));
+        }
+
+        [TestMethod]
+        public void GivenOneBilionAndOne_WhenRevert_ThenArgumentOutOfRangeExceptionShouldBeThrown()
+        {
+            var reverter = new Reverter();
+
+            Should.Throw<ArgumentOutOfRangeException>(() => reverter.Revert(1000000001));
+        }
+
+        [TestMethod]
+        public void GivenOneBilion_WhenRevert_ThenItShouldBe1365623()
+        {
+            var reverter = new Reverter();
+
+            reverter.Revert(1000000000).ShouldBe(1365623);
+        }
+
+        [TestMethod]
+        public void Given1_WhenRevert_ThenItShouldBe1()
+        {
+            var reverter = new Reverter();
+
+            reverter.Revert(1).ShouldBe(1);
+        }
+
+        [TestMethod]
+        public void Given13_WhenRevert_ThenItShouldBe11()
+        {
+            var reverter = new Reverter();
+
+            reverter.Revert(13).ShouldBe(11);
+        }
+
+        [TestMethod]
+        public void Given47_WhenRevert_ThenItShouldBe61()
+        {
+            var reverter = new Reverter();
+
+            reverter.Revert(47).ShouldBe(61);
+        }
+    }
+}
